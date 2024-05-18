c7b2ccab36f64b2f196f46a1c0488c0011005652
asiaKaplanska <kaplanska.joanna@gmail.com>
2024-05-18 14:12:45 +0200

    Prepare: Reactivity

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'jkaplanska@student.agh.edu.pl';
+    const [email, setEmail] = useState('student@student.agh.edu.pl');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    let message;
+    if (email.length > 29) {
+        message = <div>Podany adres jest zbyt długi</div>;
+    } else if (email.length < 20) {
+        message = <div>Podany adres jest zbyt krótki</div>;
+    } else {
+        message = <div>Podany adres jest prawidłowy</div>;
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to: {email.toLocaleUpperCase()}</h2>
-            <input type="text" onChange={handleChange}/>
+            <h3>{message}</h3>
+            <input type="text" value={email} onChange={handleChange}/>
         </div>
     );
 }
